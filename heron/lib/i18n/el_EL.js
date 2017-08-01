e1d4a07e484ff25ac75cbbeb89a7514c22368b8b
George Poulos <geomanagementgr@gmail.com>
2017-08-01 11:55:12 +0300

    el_EL.js greek translation
    greek translation

---
+++ heron/lib/i18n/el_EL.js
@@ -0,0 +1,286 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+Ext.namespace("Heron.i18n");
+
+/** api: (define)
+ *  module = Heron.i18n
+ *  class = Heron.i18n.dict (el_EL)
+ *  base_link = `Ext.form.ComboBox <http://dev.sencha.com/deploy/ext-3.3.1/docs/?class=Ext.form.ComboBox>`_
+ */
+
+/**
+ * Define dictionary for the GR locale.
+ * Maintained by: Heron devs gpoul2000@hotmail.com
+ */
+Heron.i18n.dict = {
+    // 0.67
+    'Active Layers': 'Ενεργά Επίπεδα',
+    'Base Layer': 'Βασικό Επίπεδο',
+    'Base Layers': 'Βασικά Επίπεδα',
+    'BaseMaps': 'Βασικοί χάρτες',
+    'Choose a Base Layer': 'Διάλεξε Βασικό Επίπεδο',
+    'Legend': 'Υπόμνημα',
+    'Feature Info': 'Πληροφορία ',
+    'Feature Data': 'Feature Data',
+    'Feature(s)': 'feature(s)',
+    'No layer selected': 'Δεν έχετε επιλέξει επίπεδο',
+    'Save Features': 'Αποθήκευση χαρακτηριστικών',
+    'Get Features': 'Πάρε τα χαρακτηριστικά',
+    'Feature information': 'Τιμές χαρακτηριστικών',
+    'No Information found': 'Δεν βρέθηκε Πληροφορία',
+    'Layer not added': 'Το επίπεδο ΔΕΝ προστέθηκε',
+    'Attribute': 'Ιδιότητα',
+    'Value': 'Τιμή',
+    'Recieving data': 'Ανάκτηση δεδομένων',
+    'Layers': 'Επίπεδα',
+    'No match': 'Δεν ταιριάζει',
+    'Loading...': 'Φόρτωση...',
+    'Bookmarks': 'Σελιδοδείκτες',
+    'Places': 'Θέσεις',
+    'Unknown': 'Άγνωστο',
+    'Feature Info unavailable': 'Αδύνατες τιμές χαρακτηριστικών',
+    'Pan': 'Pan',
+    'Leg': 'Leg',
+    'Measure length': 'Μέτρηση Μήκους',
+    'Measure area': 'Μέτρηση Εμβαδού',
+    'Length': 'Μήκος',
+    'Area': 'Επιφάνεια',
+    'Result >': 'Αποτέλεσμα >',
+    '< Search': '< Αναζήτηση',
+    'Search': 'Αναζήτηση',
+    'Search Nominatim': 'Search (OSM) data by name and address',
+    'Search OpenLS': 'Search with OpenLS service',
+    'Search PDOK': 'Type (parts of) Dutch national address',
+    'Searching...': 'Αναζήτηση...',
+    'Search Completed: ': 'Αναζήτηση ολοκληρώθηκε: ',
+    'services': 'services',
+    'service': 'service',
+    'Type Nominatim': 'Type a placename or address...',
+    'Overlays': 'Στρώσεις Επιπέδων',
+    'Waiting for': 'Αναμονή για',
+    'Warning': 'Ειδοποίηση',
+    'Zoom in': 'Εστίασε προς τα μέσα',
+    'Zoom out': 'Εστίασε προς τα έξω',
+    'Zoom to full extent': 'Zoom to full extent',
+    'Zoom previous': 'Προηγούμενη εστίαση',
+    'Zoom next': 'Επόμενη εστίαση',
+
+    // 0.68
+    'Scale': 'Κλίμακα',
+    'Resolution': 'Ανάλυση',
+    'Zoom': 'Επίπεδο Εστίασης',
+
+    // 0.70
+    'Export': 'Εξαγωγή',
+    'Choose a Display Option': 'Επιλέξτε θέαση',
+    'Display': 'Απεικόνιση',
+    'Grid': 'Πλέγμα',
+    'Tree': 'Δέντρο',
+    'XML': 'XML',
+    'Invalid export format configured: ': 'Μη έγκυρη μορφή εξαγωγής: ',
+    'No features available or non-grid display chosen': 'Δεν έχουν επιλεχθεί διαθέσιμες λειτουργίες ή δεν εμφανίζεται η οθόνη πλέγματος',
+    'Choose an Export Format': 'Επιλέξτε μια μορφή εξαγωγής',
+    'Print Visible Map Area Directly': 'Εκτυπώστε την περιοχή του ορατού χάρτη απευθείας',
+    'Direct Print Demo': 'Άμεση εκτύπωση',
+    'This is a simple map directly printed.': 'Αυτός είναι ένας απλός χάρτης που εκτυπώνεται απευθείας.',
+    'Print Dialog Popup with Preview Map': 'Εμφάνιση αναδυόμενου παραθύρου διαλόγου με χάρτη προεπισκόπησης',
+    'Print Preview': 'Προεπισκόπηση εκτύπωσης',
+    'Print Preview Demo': 'Προεπισκόπηση εκτύπωσης Demo',
+    'Error getting Print options from server: ': 'Σφάλμα επιλογών εκτύπωσης από διακομιστή: ',
+    'Error from Print server: ': 'Σφάλμα από το διακομιστή εκτύπωσης: ',
+    'No print provider url property passed in hropts.': 'Δεν μεταβιβάστηκε η ιδιότητα url του παρόχου εκτύπωσης.',
+    'Create PDF...': 'Δημιουργία PDF...',
+    'Loading print data...': 'Φόρτωση δεδομένων εκτύπωσης...',
+
+    // 0.71
+    'Go to coordinates': 'Πήγαινε στις συντεταγμένες',
+    'Go!': 'Πήγαινε!',
+    'Pan and zoom to location': 'Pan και εστίασε στην περιοχή',
+    'Enter coordinates to go to location on map': 'Εισαγωγή συντεταγμένων και πήγαινε στην περιοχή του χάρτη',
+    'Active Themes': 'Active Themes',
+    'Move up': 'Προς τα επάνω',
+    'Move down': 'Προς τα κάτω',
+    'Opacity': 'Αδιαφάνεια',
+    'Remove layer from list': 'Αφαίρεσε το επίπεδο από τη λίστα',
+    'Tools': 'Εργαλεία',
+    'Removing': 'Αφαιρεί',
+    'Are you sure you want to remove the layer from your list of layers?': 'Είστε σίγουρος ότι θέλετε να αφαιρέσετε το επίπεδο από την λίστα?',
+    'You are not allowed to remove the baselayer from your list of layers!': 'Δεν επιτρέπετε να αφαιρέσετε αυτό το επίπεδο από την λίστα!',
+
+    // 0.72
+    'Draw Features': 'Draw Features',
+
+    // 0.73
+    'Spatial Search': 'Spatial Search',
+    'Search by Drawing': 'Search by Drawing',
+    'Select the Layer to query': 'Select the Layer to query',
+    'Choose a geometry tool and draw with it to search for objects that touch it.': 'Choose a geometry tool and draw with it to search for objects that touch it.',
+    'Seconds': 'Seconds',
+    'Working on it...': 'Working on it...',
+    'Still searching, please be patient...': 'Still searching, please be patient...',
+    'Still searching, have you selected an area with too many objects?': 'Still searching, have you selected an area with too many objects?',
+    'as': 'as',
+    'Undefined (check your config)': 'Undefined (check your config)',
+    'Objects': 'Objects',
+    'objects': 'objects',
+    'Features': 'Features',
+    'features': 'features',
+    'Result': 'Αποτέλεσμα',
+    'Results': 'Αποτελέσματα',
+    'of': 'of',
+    'Using geometries from the result: ': 'Χρησιμοποιώντας γεωμετρίες από το αποτέλεσμα: ',
+    'with': 'με',
+    'Too many geometries for spatial filter: ': 'Πολλές γεωμετρίες για το χωρικό φίλτρο: ',
+    'Bookmark current map context (layers, zoom, extent)': 'Επισημάνετε το τρέχον πλαίσιο του χάρτη (επίπεδα, μεγέθυνση, έκταση)',
+    'Add a bookmark': 'Προσθήκη σελιδοδείκτη',
+    'Bookmark name cannot be empty': 'Το όνομα του σελιδοδείκτη δεν μπορεί να είναι κενό',
+    'Your browser does not support local storage for user-defined bookmarks': 'Το πρόγραμμα περιήγησης δεν υποστηρίζει τοπικό αποθηκευτικό χώρο για τους σελιδοδείκτες που έχουν οριστεί από τον χρήστη',
+    'Return to map navigation': 'Επιστροφή στον χάρτη πλοήγησης',
+    'Draw point': 'Δημιουργία Σημείου',
+    'Draw line': 'Δημιουργία Γραμμής',
+    'Draw polygon': 'Δημιουργία Πολυγώνου',
+    'Draw circle (click and drag)': 'Δημιουργία κύκλου (click and drag)',
+    'Draw Rectangle (click and drag)': 'Δημιουργία ορθογώνιου (click and drag)',
+    'Sketch is saved for use in Search by Selected Features': 'Το Σκίτσο αποθηκεύεται για χρήση στην Αναζήτηση βάσει επιλεγμένων λειτουργιών',
+    'Select a search...': 'Επιλέξτε μια αναζήτηση...',
+    'Clear': 'Καθαρισμός',
+
+    // 0.74
+    'Project bookmarks': 'Project bookmarks',
+    'Your bookmarks': 'Σελιδοδείκτες',
+    'Name': 'Όνομα',
+    'Description': 'Περιγραφή',
+    'Add': 'Προσθήκη',
+    'Cancel': 'Ακύρωση',
+    'Remove bookmark:': 'Αφαίρεση Σελιδοδείκτη:',
+    'Restore map context:': 'Επαναφορά περιβάλλοντος χάρτη:',
+    'Error: No \'BookmarksPanel\' found.': 'Σφάλμα: Δεν \'BookmarksPanel\' βρέθηκε.',
+    'Input system': 'Σύστημα εισαγωγής',
+    'Choose input system...': 'Διάλεξε σύστημα εισαγωγής...',
+    'Map system': 'Σύστημα χάρτη',
+    'X': 'X',
+    'Y': 'Y',
+    'Enter X-coordinate...': 'Εισαγωγή X-συντεταγμένη...',
+    'Enter Y-coordinate...': 'Εισαγωγή Y-συντεταγμένη...',
+    'Choose scale...': 'Επιλογή κλίμακας...',
+    'no zoom': 'όχι εστίαση',
+    'Mode': 'Mode',
+    'Remember locations': 'Θυμηθείτε θέσεις',
+    'Hide markers on close': 'Απόκρυψη δεικτών με το κλείσιμο',
+    'Remove markers on close': 'Αφαίρεση δεικτών με το κλείσιμο',
+    'Remove markers': 'Αφαίρεση δεικτών',
+    'Location': 'Τοποθεσία',
+    'Marker position: ': 'Θέση δείκτη : ',
+    'No features found': 'Δεν βρέθηκαν τιμές',
+    'Feature Info unavailable (you may need to make some layers visible)': 'Δεν υπάρχουν διαθέσιμα στοιχεία (μπορεί να χρειαστεί να εμφανίσετε ορισμένα επίπεδα)',
+    'Search by Feature Selection': 'Αναζήτηση με επιλογή Τιμών',
+    'Download': 'Λήψη',
+    'Choose a Download Format': 'Επιλογή τύπου δεδομένων λήψης',
+    'Remove all results': 'Αφαίρεσε όλα τα αποτελέσματα',
+    'Download URL string too long (max 2048 chars): ': 'Download URL string too long (max 2048 chars): ',
+
+    // 0.75
+    'Query Panel': 'Πίνακας ερωτημάτων',
+    'Cancel current search': 'Ακύρωση της τρέχουσας αναζήτησης',
+    'Search in target layer using the selected filters': 'Επιλέξτε το επίπεδο χρησιμοποιώντας τα επιλεγμένα φίλτρα',
+    'Ready': 'Έτοιμο',
+    'Search Failed': 'Η αναζήτηση απέτυχε',
+    'Search aborted': 'Η αναζήτηση χάθηκε',
+
+    // 0.76
+    'No query layers found': 'Δεν βρέθηκαν επίπεδα',
+    'Edit Layer Style': 'Επεξεργασία στυλ επιπέδου',
+    'Zoom to Layer Extent': 'Μεγέθυνση στο μέγεθος του επιπέδου',
+    'Get Layer information': 'Λάβετε πληροφορίες σχετικά με το επίπεδο',
+    'Change Layer opacity': 'Άλλαξτε την διαφάνεια του επιπέδου',
+    'Select a drawing tool and draw to search immediately': 'Επιλέξτε ένα εργαλείο σχεδίασης και σύρετε για να αναζητήσετε αμέσως',
+    'Search in': 'Αναζήτηση στο',
+    'Search Canceled': 'Αναζήτηση Ακυρώθηκε',
+    'Help and info for this example': 'Βοήθεια και πληροφορίες για αυτό το παράδειγμα',
+
+    // 1.0.1
+    'Details': 'Λεπτομέρειες',
+    'Table': 'Πίνακας',
+    'Show record(s) in a table grid': 'Εμφάνιση εγγραφών σε πλέγμα πίνακα',
+    'Show single record': 'Εμφάνιση μίας εγγραφής',
+    'Show next record': 'Εμφάνιση επόμενης εγγραφής',
+    'Show previous record': 'Εμφάνιση προηγούμενης εγγραφής',
+	'Feature tooltips' : 'Χαρακτηριστικά εργαλείων',
+	'FeatureTooltip' : 'Χαρακτηριστικά εργαλείου',
+	'Upload features from local file' : 'Ανεβάστε από τοπικό αρχείο',
+	'My Upload' : 'Αυτό που ανέβασα',
+	'Anything is allowed here' : 'Οτιδήποτε επιτρέπεται εδώ',
+	'Edit vector Layer styles' : 'Επεξεργασία στυλ διανυσματικν επιπέδων',
+	'Style Editor' : 'Επεξεργαστής στυλ',
+	'Open a map context (layers, styling, extent) from file' : 'Ανοίξτε ένα πλαίσιο χάρτη (επίπεδα, στυλ, έκταση) από αρχείο',
+	'Save current map context (layers, styling, extent) to file' : 'Αποθηκέυστε ένα πλαίσιο χάρτη (επίπεδα, στυλ, έκταση) σε αρχείο',
+	'Upload' : 'Μεταφόρτωση',
+	'Uploading file...' : 'Μεταφόρτωση αρχείου...',
+    'Change feature styles': 'Change feature styles',
+    'Error reading map file, map has not been loaded.': 'Σφάλμα κατά την ανάγνωση αρχείου χάρτη, ο χάρτης δεν έχει φορτωθεί.',
+    'Error on removing layers.': 'Σφάλμα κατά την αφαίρεση των επιπέδων.',
+    'Error loading map file.': 'Σφάλμα κατά την φόρτωση αρχείου χάρτη.',
+    'Error reading layer tree.': 'Σφάλμα κατά την ανάγνωση δομής επιπέδων.',
+
+    // 1.0.2
+    'No file specified.': 'Δεν έχει οριστεί αρχείο.',
+	'Cannot render draw controls': 'Δεν είναι δυνατή η απόδοση των draw controls',
+	'Warning - Line Length is ': 'Προειδοποίηση - Το μήκος γραμμής είναι ',
+	'You drew a line with length above the layer-maximum of ': 'Σχεδιάσατε μια γραμμή με μήκος πάνω από το μέγιστο ',
+	'Warning - Area is ': 'Προειδοποίηση - Η περιοχή είναι ',
+	'You selected an area for this layer above its maximum of ': 'Επιλέξατε μια περιοχή πάνω από το μέγιστο του ',
+	'Error creating Layer': 'Σφάλμα στη δημιουργία του επιπέδου',
+	'Error in ajax request': 'Error in ajax request',
+    'Add layers': 'Προσθήκη επιπέδου',
+    'Remove layer': 'Αφαίρεση επιπέδου',
+    'Loading map context from:': 'Φόρτωση του πλαισίου χάρτη από:',
+
+    // 1.0.3 (nothing added)
+
+    // 1.0.4
+    'INTERSECTS (default)': 'INTERSECTS (default)',
+    'WITHIN': 'WITHIN',
+    'WITHIN DISTANCE': 'WITHIN DISTANCE',
+    'CONTAINS': 'CONTAINS',
+    'Simple Time Slider': 'Simple Time Slider',
+    
+	// 1.0.6
+	'Reset': 'Reset',
+	// 'Details': 'Details',
+	'Choose Layer to select with' : 'Επιλέξτε επίπεδο για να επιλέξετε με',
+	'Select a source Layer and then draw to select objects from that layer. <br/>Then select a target Layer to search in using the geometries of the selected objects.': 'Επιλέξτε ένα Layer πηγής και στη συνέχεια σύρετε για να επιλέξετε αντικείμενα από αυτό το στρώμα. <br/> Στη συνέχεια, επιλέξτε ένα Layer στόχευση για αναζήτηση χρησιμοποιώντας τις γεωμετρίες των επιλεγμένων αντικειμένων.',
+	'Select a draw tool and draw to select objects from' : 'Επιλέξτε ένα εργαλείο σχεδιασμού και σύρετε για να επιλέξετε αντικείμενα από',
+	'Select a target layer to search using the geometries of the selected objects': 'Επιλέξτε ένα επίπεδο προορισμού για αναζήτηση χρησιμοποιώντας τις γεωμετρίες των επιλεγμένων αντικειμένων',
+	'objects selected from "': 'Αντικείμενα που επιλέγονται από "',
+	'Search in target layer using the feature-geometries from the selection' : 'Search in target layer using the feature-geometries from the selection',
+	'Select the spatial operator (optional) and use the Search button to start your search.': 'Επιλέξτε την αναζήτηση με βάση τον χάρτη (προαιρετικό) και χρησιμοποιήστε το κουμπί Αναζήτηση για να ξεκινήσετε την αναζήτησή σας.',
+	'Type of Search': 'Τύπος αναζήτησης',
+	'This field is required': 'Αυτό το πεδίο απαιτείται',
+	'Select a drawing tool and draw to search immediately.': 'Επιλέξτε ένα εργαλείο σχεδίασης και σύρετε για να αναζητήσετε αμέσως.',
+	'Actions': 'Ενέργειες',
+	'Cancel ongoing search': 'Ακύρωση της τρέχουσας αναζήτησης',
+	'Distance of Search': 'Απόσταση αναζήτησης',
+	'Choose a Layer': 'Επιλέξτε ένα επίπεδο',
+	'No objects selected': 'Δεν έχουν επιλεγεί αντικείμενα',
+	'Too many geometries for spatial filter': 'Πολλές γεωμετρίες για το χωρικό φίλτρο',
+	'Build your own searches': 'Δημιουργήστε τις δικές σας αναζητήσεις',
+	// 'Get Layer information': 'Πληροφορία επιπέδου',
+	// 'Edit Layer Style': 'Επεξεργασία στυλ επιπέδου',
+	'Style Editor (Vector)': 'Επεξεργαστής στυλ (Vector)',
+
+    // 1.0.7
+    'OverviewMap': 'Εναλλαγή επισκόπησης',
+    'Κάντε κλικ στο χάρτη για να δείτε την τοποθεσία στο Google StreetView': 'Click on map to view location in Google StreetView'
+};
